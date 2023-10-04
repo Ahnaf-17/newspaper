05db219db39c835eab14673a035cf911a94f966b
unknown <ahnaf.ahmed.42173@gmail.com>
2023-10-04 12:48:25 +0600

    navbar and header fixed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,27 @@
 import Header from "../Shared_pages/Header/Header";
+import LeftNav from "../Shared_pages/LeftNav/LeftNav";
 import Navbar from "../Shared_pages/Navbar/Navbar";
+import RightNav from "../Shared_pages/RightNav/RightNav";
+import BreakingNews from "./BreakingNews";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
-            <h2 className="text-2xl font-poppins">home theke bolchi</h2>
+
+            <div className="grid md:grid-cols-4 grid-cols-1 gap-6 ">
+                <div className="text-center border">
+                    <LeftNav></LeftNav>
+                </div>
+                <div className="md:col-span-2 text-center border">
+                    <h2>Hot news</h2>
+                </div>
+                <div className="text-center border">
+                    <RightNav></RightNav>
+                </div>
+            </div>
         </div>
     );
 };
